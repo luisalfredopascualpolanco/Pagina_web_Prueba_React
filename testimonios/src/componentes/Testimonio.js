ff19b2fa93624d03b747118a2e8e3bf5fedc0ce5
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2022-05-01 21:11:08 -0700

    Update Testimonio.js

--- testimonios/src/componentes/Testimonio.js
+++ testimonios/src/componentes/Testimonio.js
@@ -4,7 +4,7 @@ import '../hojas-de-estilo/Testimonio.css';
 function Testimonio(props) {
   return(
     <div className="contenedor-testimonio">
-      <img className="imagen-testimonio" src={require(`../imagenes/${props.imagen}.png`)} alt="Foto de Emma"/>
+      <img className="imagen-testimonio" src={require(`../imagenes/${props.imagen}.png`)} alt={props.nombre}/>
       <div className="contenedor-texto-testimonio">
         <p className="nombre-testimonio">
           <strong>{props.nombre}</strong> en {props.pais}
